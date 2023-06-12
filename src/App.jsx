91beffa07d23212b46265881e908124f51865326
zannuhandiranu27 <59725059+zannuhandiranu27@users.noreply.github.com>
2023-06-12 23:14:43 +0700

    tes
    tes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Registrasi from "./pages/Registrasi";
+import Home from "./pages/LandingPage";
 export const apiEndPoint = "https://647c25b9c0bae2880ad06ad3.mockapi.io/";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 		<div style={{ fontFamily: "Poppins, sans-serif" }}>
 			<BrowserRouter>
 				<Routes>
+					<Route path="/" element={<Home />} />
 					<Route path="/" element={<Login />} />
 					<Route path="/registrasi" element={<Registrasi />} />
 				</Routes>
